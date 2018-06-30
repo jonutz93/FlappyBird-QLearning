e672450716b8cc2388c52a62ad539eed1cee0584
jonutz93 <manta.ionut@yahoo.com>
2018-06-30 23:32:10 +0300

    +Added constants.py and Controller.py

---
+++ Constants.py
@@ -0,0 +1,9 @@
+#constants such as the name of the game window, FPS
+GameName = "FlappyBird"
+FPS = 30
+
+#posible inputs according to http://www.gamespp.com/directx/directInputKeyboardScanCodes.html
+W = 0x11
+inputsList = [W]
+
+
---
+++ Controller.py
@@ -0,0 +1,62 @@
+import ctypes
+import time
+import Constants
+from win32gui import GetWindowText, GetForegroundWindow
+
+SendInput = ctypes.windll.user32.SendInput
+
+# C struct redefinitions 
+PUL = ctypes.POINTER(ctypes.c_ulong)
+class KeyBdInput(ctypes.Structure):
+    _fields_ = [("wVk", ctypes.c_ushort),
+                ("wScan", ctypes.c_ushort),
+                ("dwFlags", ctypes.c_ulong),
+                ("time", ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+
+class HardwareInput(ctypes.Structure):
+    _fields_ = [("uMsg", ctypes.c_ulong),
+                ("wParamL", ctypes.c_short),
+                ("wParamH", ctypes.c_ushort)]
+
+class MouseInput(ctypes.Structure):
+    _fields_ = [("dx", ctypes.c_long),
+                ("dy", ctypes.c_long),
+                ("mouseData", ctypes.c_ulong),
+                ("dwFlags", ctypes.c_ulong),
+                ("time",ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+
+class Input_I(ctypes.Union):
+    _fields_ = [("ki", KeyBdInput),
+                 ("mi", MouseInput),
+                 ("hi", HardwareInput)]
+
+class Input(ctypes.Structure):
+    _fields_ = [("type", ctypes.c_ulong),
+                ("ii", Input_I)]
+
+# Actuals Functions
+
+#function that presses a key on the active window.
+#Future me should find to send the input to a specific window istead of the active one
+#it only sends the input if the active window is Constants.GameName
+def PressKey(hexKeyCode):
+    if GetWindowText(GetForegroundWindow()) == Constants.GameName:
+        extra = ctypes.c_ulong(0)
+        ii_ = Input_I()
+        ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008, 0, ctypes.pointer(extra) )
+        x = Input( ctypes.c_ulong(1), ii_ )
+        ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+        time.sleep(1/Constants.FPS)
+
+def ReleaseKey(hexKeyCode):
+    if GetWindowText(GetForegroundWindow()) == Constants.GameName:
+        extra = ctypes.c_ulong(0)
+        ii_ = Input_I()
+        ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008 | 0x0002, 0, ctypes.pointer(extra) )
+        x = Input( ctypes.c_ulong(1), ii_ )
+        ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+        time.sleep(1/Constants.FPS)
+
+
